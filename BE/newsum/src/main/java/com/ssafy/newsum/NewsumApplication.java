b8471beca53cffa7b780552ecdfc69160efae611
jihee9945 <jihee9945@naver.com>
2023-09-15 13:40:44 +0900

    feat: upload project

---
+++ BE/newsum/src/main/java/com/ssafy/newsum/NewsumApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.newsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsumApplication.class, args);
+	}
+
+}
