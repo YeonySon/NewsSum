a830a76978ff5ed3e6c17426022e3405903ea37a
손승연 <zzangookd@gmail.com>
2023-09-20 17:01:44 +0900

    close 연습

--- BE/newsum/src/main/java/com/ssafy/newsum/domain/news/service/NewsService.java
+++ BE/newsum/src/main/java/com/ssafy/newsum/domain/news/service/NewsService.java
@@ -139,7 +139,7 @@ public class NewsService {
         // 전체분야
         if (categoryId == 0) {
 
-            // 인기도 순
+            // 인기도   fff순
             if (optionId == 1) {
                 newsList = newsRepository.selectAllPopular();
             }
