beae008d10e0e2e7d755ef49684afbfdbf307bfa
jungmin0049 <jungmin0049@gmail.com>
2023-09-27 14:56:40 +0900

    fix: nav router 추가

--- FE/newsum/src/components/util/Navbar.tsx
+++ FE/newsum/src/components/util/Navbar.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+//icon import
 import { FaPlayCircle, FaRegPlayCircle } from 'react-icons/fa';
 import { RiFilePaperFill, RiFilePaperLine } from 'react-icons/ri';
 import { FaRegUserCircle, FaUserCircle } from 'react-icons/fa';
+
+// loginModal component import
+import LoginModal from '../../page/login/loginModal';
+
+// recoil
+import { useRecoilState } from 'recoil';
+import { LoginModalIsOpenAtom } from '../../recoil/atoms/LoginModalAtom';
 
 export const NavBar = styled.div`
   padding: 0;
@@ -23,6 +34,10 @@ export const NavBar = styled.div`
 
   .active {
     color: #0583f2;
+  }
+
+  .deactive a {
+    color: #394867;
   }
 
   .nav {
@@ -107,11 +122,18 @@ export const NavBar = styled.div`
       flex-direction: column;
       justify-content: center;
       transform: translate(20%, 0%);
+      position: relative;
     }
     .nav strong {
-      display: inline;
+      position: absolute;
+      display: inline-block;
+      text-align: center;
+      width: 100px;
       font-size: 1rem;
-      transform: translate(-35%, 0%);
+
+      /* overflow: auto; */
+      white-space: nowrap;
+      transform: translate(-73%, 160%);
     }
   }
 `;
@@ -174,36 +196,51 @@ const Button = styled.div`
 `;
 
 function Navbar({ nav }) {
-  const [userInfo, setUserInfo] = useState(true);
+  const [userId, setUserId] = useState('');
 
-  function short() {}
+  // login
+  const [loginModalOpen, setLoginModalOpen] = useRecoilState(LoginModalIsOpenAtom);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-  function news() {}
-
-  function mypage() {}
+  // modal FadeIn, FadeOut를 위한 시간 지연
+  useEffect(() => {
+    if (loginModalOpen) {
+      setIsAnimating(true);
+    } else {
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 300);
+    }
+  }, [loginModalOpen]);
 
   function login() {
-    setUserInfo(true);
+    setLoginModalOpen(true);
   }
 
   return (
     <div>
       <NavBar>
         <ul className="nav">
-          <li className={nav == 'short' ? 'active' : ''} onClick={short}>
-            {nav == 'short' ? <FaPlayCircle /> : <FaRegPlayCircle />}
-            <strong>short</strong>
+          <li className={nav == 'short' ? 'active' : 'deactive'}>
+            <NavLink to="/short">
+              {nav == 'short' ? <FaPlayCircle /> : <FaRegPlayCircle />}
+              <strong>short</strong>
+            </NavLink>
           </li>
-          <li className={nav == 'news' ? 'active' : ''} onClick={news}>
-            {nav == 'news' ? <RiFilePaperFill /> : <RiFilePaperLine />}
-            <strong>news</strong>
+          <li className={nav == 'news' ? 'active' : 'deactive'}>
+            <NavLink to="/news">
+              {nav == 'news' ? <RiFilePaperFill /> : <RiFilePaperLine />}
+              <strong>news</strong>
+            </NavLink>
           </li>
-          <li className={nav == 'mypage' ? 'active' : ''} onClick={mypage}>
-            {nav == 'mypage' ? <FaUserCircle /> : <FaRegUserCircle />}
-            <strong>my page</strong>
+          <li className={nav == 'mypage' ? 'active' : 'deactive'}>
+            <NavLink to="/mypage">
+              {nav == 'mypage' ? <FaUserCircle /> : <FaRegUserCircle />}
+              <strong>my page</strong>
+            </NavLink>
           </li>
         </ul>
-        <Button onClick={login}>Log In</Button>
+        {userId == '' && <Button onClick={login}>Log In</Button>}
 
         <Footer>
           <div>
@@ -220,6 +257,7 @@ function Navbar({ nav }) {
           </div>
         </Footer>
       </NavBar>
+      {(loginModalOpen || isAnimating) && <LoginModal />}
     </div>
   );
 }
