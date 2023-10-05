e54a4bffe6d835cbe855808412fb453bc89f982d
jungmin0049 <jungmin0049@gmail.com>
2023-10-06 03:24:50 +0900

    fix:접근불가 에러 수정

--- FE/newsum/src/components/mypage/KeywordComponent.tsx
+++ FE/newsum/src/components/mypage/KeywordComponent.tsx
@@ -1,67 +1,65 @@
 // 라이브러리
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import cookie from 'react-cookies';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import cookie from "react-cookies";
 
 // recoil import
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { SignUpAtom } from '../../recoil/atoms/SignUpAtom';
-import { MyInfoAtom } from '../../recoil/atoms/MyInfoAtom';
+import { useRecoilState, useRecoilValue } from "recoil";
+import { SignUpAtom } from "../../recoil/atoms/SignUpAtom";
+import { MyInfoAtom } from "../../recoil/atoms/MyInfoAtom";
 
 // axios instance
-import { BaseInstance } from '../../hook/AxiosInstance';
+import { BaseInstance } from "../../hook/AxiosInstance";
 
 // 페이지 입장 권한 확인
-import { CheckCookie } from '../../hook/token';
+import { CheckCookie } from "../../hook/token";
 
-import { 
+import {
   NavButtonBox,
   NavButton,
-  SignUpPage, 
-  Container, 
+  SignUpPage,
+  Container,
   LogoTag,
   SelectContainer,
   SelectBox,
   SelectItem,
   ContentWordTag,
   InputWordTag,
-
   DivLine,
   SelectedContainer,
   SelectedBox,
   SelectedItem,
   DeleteButton,
-
   NextButton,
-} from '../signup/signUp';
+} from "../signup/signUp";
 
 // pageType === 0은 SignUP
 // pageType === 1는 myInfo/keyword
 function KeywordComponent({ pageType }) {
   const navigate = useNavigate();
   const [formData, setFormData] = useRecoilState(SignUpAtom);
-  const userId = useRecoilValue(MyInfoAtom);
-  const buttonName = ['다음', '저장'];
+  const [userId, setMyInfo] = useRecoilState(MyInfoAtom);
 
-  const [items, setItems] = useState<string[]>([])
+  const buttonName = ["다음", "저장"];
+
+  const [items, setItems] = useState<string[]>([]);
   const [checkedList, setCheckedList] = useState<number[]>(() => {
     if (!!formData.tech) {
-      return formData.tech.map(item => item - 1);
+      return formData.tech.map((item) => item - 1);
     }
-    return []
+    return [];
   });
   const [itemStates, setItemStates] = useState<boolean[]>(() => {
     const initialStates = new Array(items.length).fill(false);
     if (!!formData.tech) {
-      formData.tech.forEach((idx) => initialStates[idx - 1] = true)
+      formData.tech.forEach((idx) => (initialStates[idx - 1] = true));
     }
     return initialStates;
-  })
-
+  });
 
   const handleCheckedList = (index: number, isChecked: boolean) => {
     if (isChecked) {
-      setCheckedList((prev) => [...prev, index])
+      setCheckedList((prev) => [...prev, index]);
       return;
     }
     if (!isChecked && checkedList.includes(index)) {
@@ -72,63 +70,67 @@ function KeywordComponent({ pageType }) {
   };
 
   const handleItemClick = (index: number) => {
-    if (checkedList.length < 5 || checkedList.includes(index) ) {
-      const newitemStates = [...itemStates]
-      newitemStates[index] = !newitemStates[index]
-      setItemStates(newitemStates)
-      handleCheckedList(index, newitemStates[index])
-    } 
-  }
+    if (checkedList.length < 5 || checkedList.includes(index)) {
+      const newitemStates = [...itemStates];
+      newitemStates[index] = !newitemStates[index];
+      setItemStates(newitemStates);
+      handleCheckedList(index, newitemStates[index]);
+    }
+  };
 
   const finalCheck = async () => {
     if (checkedList.length === 0) {
-      alert('하나 이상의 기술스택을 선택해주세요')
+      alert("하나 이상의 기술스택을 선택해주세요");
       return;
     }
 
     // 관심기술 recoil에 저장
-    const newData = checkedList.map(item => item + 1)
-    const data = {tech: newData}
-    setFormData((prev) => ({...prev, ...data}))
+    const newData = checkedList.map((item) => item + 1);
+    const data = { tech: newData };
+    setFormData((prev) => ({ ...prev, ...data }));
 
     if (pageType === 0) {
-      navigate('/signup/3') // page 이동
-    } 
-    
+      navigate("/signup/3"); // page 이동
+    }
+
     if (pageType === 1) {
       const requestBodyJSON = JSON.stringify(newData);
       // 쿠키 불러오기
       const headers = {
-        'Authorization': `Bearer ${cookie.load('accessToken')}`
-      }
-      await BaseInstance.patch(`/api/mypage/tech/${userId}`, requestBodyJSON, { headers: headers })
-      .then((resposne) => {
-        console.log(resposne)
-        alert('변경사항이 저장되었습니다.')
+        Authorization: `Bearer ${cookie.load("accessToken")}`,
+      };
+      await BaseInstance.patch(`/api/mypage/tech/${userId}`, requestBodyJSON, {
+        headers: headers,
       })
-      .catch((error) => {
-        console.log(error)
-      })
+        .then((resposne) => {
+          console.log(resposne);
+          alert("변경사항이 저장되었습니다.");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }
+  };
 
   useEffect(() => {
     // 로그인 여부 확인
-    CheckCookie();
+    CheckCookie(setMyInfo);
 
     // 쿠키 불러오기
     const headers = {
-      'Authorization': `Bearer ${cookie.load('accessToken')}`
-    }
+      Authorization: `Bearer ${cookie.load("accessToken")}`,
+    };
 
     // 서버에 데이터 요청
     const responseData = async () => {
-      await BaseInstance.get('/api/user/techstack')
+      await BaseInstance.get("/api/user/techstack")
         .then((response) => {
-          setItems(response.data.data.map((item: {tsName: string}) => item.tsName))
+          setItems(
+            response.data.data.map((item: { tsName: string }) => item.tsName)
+          );
         })
-        .catch((error) => [] as string[])
-    }
+        .catch((error) => [] as string[]);
+    };
 
     // 서버에 내 정보조회 요청
     // const responseMyInfoData = async () => {
@@ -146,24 +148,25 @@ function KeywordComponent({ pageType }) {
     // }
     // responseMyInfoData();
 
-    responseData()
-  }, [])
-
+    responseData();
+  }, []);
 
   return (
     <SignUpPage>
-      {pageType === 0 &&
-        <LogoTag><p>NewSum</p></LogoTag>
-      }
+      {pageType === 0 && (
+        <LogoTag>
+          <p>NewSum</p>
+        </LogoTag>
+      )}
       <Container>
-        {pageType === 0 &&
+        {pageType === 0 && (
           <NavButtonBox>
             <NavButton $isActive={false} />
             <NavButton $isActive={true} />
             <NavButton $isActive={false} />
           </NavButtonBox>
-        }
-        
+        )}
+
         <SelectContainer>
           <ContentWordTag>
             <p>관심이 있는</p>
@@ -176,7 +179,7 @@ function KeywordComponent({ pageType }) {
               <SelectItem
                 key={idx}
                 $isActive={itemStates[idx]}
-                onClick={()=>handleItemClick(idx)}
+                onClick={() => handleItemClick(idx)}
               >
                 {item}
               </SelectItem>
@@ -184,22 +187,25 @@ function KeywordComponent({ pageType }) {
           </SelectBox>
         </SelectContainer>
 
-        <InputWordTag><p>선택한 기술 스택</p></InputWordTag>
+        <InputWordTag>
+          <p>선택한 기술 스택</p>
+        </InputWordTag>
         <DivLine />
         <SelectedContainer>
           {checkedList.map((item, idx) => (
             <SelectedBox key={idx}>
               <SelectedItem>{items[item]}</SelectedItem>
-              <DeleteButton onClick={()=>handleItemClick(item)}>x</DeleteButton>
+              <DeleteButton onClick={() => handleItemClick(item)}>
+                x
+              </DeleteButton>
             </SelectedBox>
-
           ))}
         </SelectedContainer>
 
-        <NextButton onClick={ finalCheck }>{buttonName[pageType]}</NextButton>
+        <NextButton onClick={finalCheck}>{buttonName[pageType]}</NextButton>
       </Container>
     </SignUpPage>
-  )
+  );
 }
 
 export default KeywordComponent;
--- FE/newsum/src/hook/token.tsx
+++ FE/newsum/src/hook/token.tsx
@@ -1,15 +1,15 @@
-import cookie from 'react-cookies';
+import cookie from "react-cookies";
 
 // recoil import
-import { useSetRecoilState } from 'recoil';
-import { MyInfoAtom } from '../recoil/atoms/MyInfoAtom';
+import { useSetRecoilState } from "recoil";
+import { MyInfoAtom } from "../recoil/atoms/MyInfoAtom";
 
-export function CheckCookie() {
-  const setMyinfo = useSetRecoilState(MyInfoAtom);
+export function CheckCookie(setMyinfo) {
+  // const setMyinfo = useSetRecoilState(MyInfoAtom);
 
-  if (cookie.load('accessToken') === undefined) {
-    setMyinfo(0)
-    alert('로그인이 필요한 서비스입니다.')
-    window.location.href = '/news'
-  } 
+  if (cookie.load("accessToken") === undefined) {
+    setMyinfo(0);
+    alert("로그인이 필요한 서비스입니다.");
+    window.location.href = "/news";
+  }
 }
