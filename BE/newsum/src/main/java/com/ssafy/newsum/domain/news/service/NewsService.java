f068a3d11b8b1ac943142d013b72029755eec3b2
jihee9945 <jihee9945@naver.com>
2023-10-05 16:14:48 +0900

    fix: param import update

--- BE/newsum/src/main/java/com/ssafy/newsum/domain/news/service/NewsService.java
+++ BE/newsum/src/main/java/com/ssafy/newsum/domain/news/service/NewsService.java
@@ -27,9 +27,11 @@ import com.ssafy.newsum.domain.users.entity.User;
 import com.ssafy.newsum.domain.users.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NewsService {
 
 	private final NewsRepository newsRepository;
