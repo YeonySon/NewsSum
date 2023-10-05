c79d761893265c609acb5b4b6ecf64809ea8ee80
jihee9945 <jihee9945@naver.com>
2023-10-05 13:29:58 +0900

    docs: 코드 수정

--- BE/newsum/src/main/java/com/ssafy/newsum/domain/users/service/UserService.java
+++ BE/newsum/src/main/java/com/ssafy/newsum/domain/users/service/UserService.java
@@ -163,8 +163,6 @@ public class UserService {
 	public User updatePassword(Integer id, UserRequestDto userRequestDto) {
 		userRequestDto.setId(id);
 		User user = userRequestDto.toEntity(userRequestDto);
-		log.info("userId : {}", id);
-		log.info("userpassword : {}", userRequestDto.getPassword());
 		//1. 비밀번호 암호화
 		user.passwordEncode(passwordEncoder);
 
