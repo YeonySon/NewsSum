73877446ee0a6a079b301e7cbf04f9c67784449b
손승연 <zzangookd@gmail.com>
2023-10-06 11:16:38 +0900

    fix

--- BE/newsum/src/main/java/com/ssafy/newsum/domain/recommendnews/repository/RecommendnewsRepository.java
+++ BE/newsum/src/main/java/com/ssafy/newsum/domain/recommendnews/repository/RecommendnewsRepository.java
@@ -10,15 +10,11 @@ import java.util.List;
 
 public interface RecommendnewsRepository extends JpaRepository<RecommendNews, Integer> {
 
-<<<<<<< HEAD
+
     @Modifying
     @Query("update RecommendNews r set r.isRead = 1 where r.newsId =:newsId and r.usrId =:userId")
     void updateRecommendIsRead(@Param("newsId") Integer newsId, @Param("userId") Integer userId);
-=======
-	@Modifying
-	@Query("update RecommendNews r set r.isRead = 1 where r.newsId =:newsId and r.usrId =:userId")
-	void updateRecommendIsRead(@Param("newsId") Integer newsId, @Param("userId") Integer userId);
->>>>>>> 590ffc0817db68a78a9f5b6ba3944f9e2e54d709
+
 
     // 추천기사 사용자 id 기준으로 조회
     @Query("select r from RecommendNews r where r.usrId =:userId")
